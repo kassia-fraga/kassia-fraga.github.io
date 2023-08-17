a9ab91ddda4572ff34670793af2f0ffdb573ba12
Kassia Fraga <kassiafraga7@gmail.com>
2023-08-17 11:51:26 -0300

    Test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -23,6 +23,11 @@ import image5 from '@/images/photos/image-5.jpg'
 import { formatDate } from '@/lib/formatDate'
 import { getAllArticles } from '@/lib/getAllArticles'
 
+
+
+import { getClient } from "@/lib/client";
+import { authorQuery } from '@/lib/queries'
+
 function MailIcon(props) {
   return (
     <svg
@@ -246,42 +251,54 @@ function Photos() {
 
 export default async function Home() {
   let articles = (await getAllArticles()).slice(0, 4)
+  const { data } = await getClient().query({
+    query: authorQuery,
+    variables: { slug: "leanne-graham" },
+    context: { fetchOptions: { next: { revalidate: 5 } } } // revalidate every 5 seconds
+  });
 
   return (
     <>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            { data.author.intro }
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            { data.author.bio }
           </p>
-          <div className="mt-6 flex gap-6">
-            <SocialLink
-              href="https://twitter.com"
-              aria-label="Follow on Twitter"
-              icon={TwitterIcon}
-            />
-            <SocialLink
-              href="https://instagram.com"
-              aria-label="Follow on Instagram"
-              icon={InstagramIcon}
-            />
-            <SocialLink
-              href="https://github.com"
-              aria-label="Follow on GitHub"
-              icon={GitHubIcon}
-            />
-            <SocialLink
-              href="https://linkedin.com"
-              aria-label="Follow on LinkedIn"
-              icon={LinkedInIcon}
-            />
-          </div>
+          {
+            data.author.social && (
+              <div className="mt-6 flex gap-6">
+                {
+                  data.author.social.twitterUrl && (
+                    <SocialLink
+                      href={data.author.social.twitterUrl}
+                      aria-label="Follow on Twitter"
+                      icon={TwitterIcon}
+                    />
+                  )
+                }
+
+                {/* <SocialLink
+                  href="https://instagram.com"
+                  aria-label="Follow on Instagram"
+                  icon={InstagramIcon}
+                />
+                <SocialLink
+                  href="https://github.com"
+                  aria-label="Follow on GitHub"
+                  icon={GitHubIcon}
+                />
+                <SocialLink
+                  href="https://linkedin.com"
+                  aria-label="Follow on LinkedIn"
+                  icon={LinkedInIcon}
+                /> */}
+              </div>
+            )
+          }
+
         </div>
       </Container>
       <Photos />
